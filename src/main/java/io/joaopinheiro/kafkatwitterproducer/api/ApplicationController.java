ccb3a347c7f5409f39d00b3f0c32b198a3f90a4c
Joao Pinheiro <joao.pinheiro@holidu.com>
2020-11-01 13:23:12 +0100

    Add slf4j dependency.
    Add simple rest controller

---
+++ src/main/java/io/joaopinheiro/kafkatwitterproducer/api/ApplicationController.java
@@ -0,0 +1,15 @@
+package io.joaopinheiro.kafkatwitterproducer.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api")
+public class ApplicationController {
+
+    @GetMapping("/status")
+    public String health() {
+        return "OK";
+    }
+}
