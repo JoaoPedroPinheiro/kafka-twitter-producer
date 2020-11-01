73431878b85452a86d2e7400ed86e311148d85c6
Joao Pinheiro <joao.pinheiro@holidu.com>
2020-11-01 14:38:46 +0100

    Add Config for Twitter client

--- src/main/java/io/joaopinheiro/kafkatwitterproducer/twitter/client/TwitterClientConfig.java
+++ src/main/java/io/joaopinheiro/kafkatwitterproducer/twitter/client/TwitterClientConfig.java
@@ -1,16 +1,16 @@
 package io.joaopinheiro.kafkatwitterproducer.twitter.client;
 
-import lombok.Value;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Value
+@Data
 @Component
 @ConfigurationProperties("twitter")
 public class TwitterClientConfig {
 
     String consumerKey;
     String consumerSecret;
-    String consumerToken;
-    String secret;
+    String token;
+    String tokenSecret;
 }
