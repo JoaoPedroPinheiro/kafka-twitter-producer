456889939392b3173785e94218372fea517c5776
Joao Pinheiro <joao.pinheiro@holidu.com>
2020-11-01 03:03:18 +0100

    initial commit

---
+++ src/main/java/io/joaopinheiro/kafkatwitterproducer/KafkaTwitterProducerApplication.java
@@ -0,0 +1,13 @@
+package io.joaopinheiro.kafkatwitterproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTwitterProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTwitterProducerApplication.class, args);
+	}
+
+}
